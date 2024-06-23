8d6e4908114f3e1d87c6729a0ff1b2def9ae85b8
Ayush Sharma <aayush240204@gmail.com>
2024-06-24 02:07:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -6,21 +6,56 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
-	    int index = -1;
-	    int max_cnt = -1;
-	    for(int i = 0; i<n; i++){
-	        int cntRow = -1;
-	        for(int j = 0; j<m;j++){
-	            cntRow += arr[i][j];
-	        }
-	        if(cntRow > max_cnt){
-	            max_cnt = cntRow;
-	            index = i;
-	        }
-	    }
-	    return index;
-	}
+// Bruteforce - Linear Search TC - O(n*m) 
+// 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+// 	    int index = -1;
+// 	    int max_cnt = -1;
+// 	    for(int i = 0; i<n; i++){
+// 	        int cntRow = -1;
+// 	        for(int j = 0; j<m;j++){
+// 	            cntRow += arr[i][j];
+// 	        }
+// 	        if(cntRow > max_cnt){
+// 	            max_cnt = cntRow;
+// 	            index = i;
+// 	        }
+// 	    }
+// 	    return index;
+// 	}
+    
+    int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m) {
+    int cnt_max = 0;
+    int index = -1;
+
+    //traverse the rows:
+    for (int i = 0; i < n; i++) {
+        // get the number of 1's:
+        int cnt_ones = m - lowerBound(matrix[i], m, 1);
+        if (cnt_ones > cnt_max) {
+            cnt_max = cnt_ones;
+            index = i;
+        }
+    }
+    return index;
+}
 
 };
 
