69ee66974d3f20232c2876e12106e249a064e283
Ayush Sharma <aayush240204@gmail.com>
2024-07-29 03:00:19 +0530

    Update dijkstra-algorithm.cpp

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -6,45 +6,84 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to find the shortest distance of all the vertices
-    //from the source vertex S.
-    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
-    {
-        // craeting expolored and dist arr
-        vector<bool> Explored(V,0); // bool vector for explore track
-        vector<int> dist(V,INT_MAX); 
-        dist[S] = 0; // dist from Source to source
-        int count = V;
-        while(count--){
-            // selecting a node which is unexplored and its dist is minimum
-            int node = -1; // initial node 
-            int value = INT_MAX; // initial value
+	// Method 1
+    // vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    // {
+    //     // creating expolored and dist arr
+    //     vector<bool> Explored(V,0); // bool vector for explore track
+    //     vector<int> dist(V,INT_MAX); 
+    //     dist[S] = 0; // dist from Source to source
+    //     int count = V;
+    //     while(count--){
+    //         // selecting a node which is unexplored and its dist is minimum
+    //         int node = -1; // initial node 
+    //         int value = INT_MAX; // initial value
             
-            for(int i = 0; i<V; i++){
-                if(!Explored[i] && value > dist[i]){
-                    // initializing the new values
-                    node = i;
-                    value = dist[i];
-                }
-            }
+    //         for(int i = 0; i<V; i++){
+    //             if(!Explored[i] && value > dist[i]){
+    //                 // initializing the new values
+    //                 node = i;
+    //                 value = dist[i];
+    //             }
+    //         }
             
-            Explored[node] = 1; // marking node as explored
+    //         Explored[node] = 1; // marking node as explored
+            
+    //         // relax the edges
+    //         for(int v = 0 ; v < adj[node].size(); v++){
+    //             int neighbour = adj[node][v][0];
+    //             int weight = adj[node][v][1];
+                
+    //             // condition check
+    //             if(!Explored[neighbour] &&
+    //             weight + dist[node] < dist[neighbour]){
+    //                 dist[neighbour] = weight + dist[node];
+    //             }
+    //         }
+    //     }
+        
+    //     return dist;
+        
+    // }
+    
+    // Method 2  -> Using Min Heap
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S){
+        // creating explored and visited arr
+        vector<bool> Explored(V,0);
+        vector<int> dist(V,INT_MAX);
+        dist[S] = 0; // initializing S dist as 0
+        
+        // min heap for operations
+        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        
+        // initalize the pq
+        pq.push({0,S});  // {dist,node}
+        
+        // we do operations in queue
+        while(!pq.empty()){
+            int node = pq.top().second;
+            pq.pop();
+            
+            // if already explored
+            if(Explored[node]==1)  continue;
+            
+            Explored[node] = 1; // make id explored
             
             // relax the edges
-            for(int v = 0 ; v < adj[node].size(); v++){
-                int neighbour = adj[node][v][0];
-                int weight = adj[node][v][1];
+            for(auto & v : adj[node]){
+                int neighbour = v[0];
+                int weight = v[1];
                 
-                // condition check
+                // condt
                 if(!Explored[neighbour] &&
-                weight + dist[node] < dist[neighbour]){
-                    dist[neighbour] = weight + dist[node];
+                dist[node] + weight < dist[neighbour]){
+                    dist[neighbour] = dist[node] + weight;
+                    pq.push({dist[neighbour] , neighbour});
                 }
             }
         }
         
         return dist;
-        
     }
 };
 
