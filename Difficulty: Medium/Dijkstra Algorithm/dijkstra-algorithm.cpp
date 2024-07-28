f7c87581342fda25fa37beba2860fbebf7d491b5
Ayush Sharma <aayush240204@gmail.com>
2024-07-29 03:36:51 +0530

    Update dijkstra-algorithm.cpp

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -7,44 +7,44 @@ class Solution
 {
 	public:
 	// Method 1
-    // vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
-    // {
-    //     // creating expolored and dist arr
-    //     vector<bool> Explored(V,0); // bool vector for explore track
-    //     vector<int> dist(V,INT_MAX); 
-    //     dist[S] = 0; // dist from Source to source
-    //     int count = V;
-    //     while(count--){
-    //         // selecting a node which is unexplored and its dist is minimum
-    //         int node = -1; // initial node 
-    //         int value = INT_MAX; // initial value
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // creating expolored and dist arr
+        vector<bool> Explored(V,0); // bool vector for explore track
+        vector<int> dist(V,INT_MAX); 
+        dist[S] = 0; // dist from Source to source
+        int count = V;
+        while(count--){
+            // selecting a node which is unexplored and its dist is minimum
+            int node = -1; // initial node 
+            int value = INT_MAX; // initial value
             
-    //         for(int i = 0; i<V; i++){
-    //             if(!Explored[i] && value > dist[i]){
-    //                 // initializing the new values
-    //                 node = i;
-    //                 value = dist[i];
-    //             }
-    //         }
+            for(int i = 0; i<V; i++){
+                if(!Explored[i] && value > dist[i]){
+                    // initializing the new values
+                    node = i;
+                    value = dist[i];
+                }
+            }
             
-    //         Explored[node] = 1; // marking node as explored
+            Explored[node] = 1; // marking node as explored
             
-    //         // relax the edges
-    //         for(int v = 0 ; v < adj[node].size(); v++){
-    //             int neighbour = adj[node][v][0];
-    //             int weight = adj[node][v][1];
+            // relax the edges
+            for(int v = 0 ; v < adj[node].size(); v++){
+                int neighbour = adj[node][v][0];
+                int weight = adj[node][v][1];
                 
-    //             // condition check
-    //             if(!Explored[neighbour] &&
-    //             weight + dist[node] < dist[neighbour]){
-    //                 dist[neighbour] = weight + dist[node];
-    //             }
-    //         }
-    //     }
+                // condition check
+                if(!Explored[neighbour] &&
+                weight + dist[node] < dist[neighbour]){
+                    dist[neighbour] = weight + dist[node];
+                }
+            }
+        }
         
-    //     return dist;
+        return dist;
         
-    // }
+    }
     
     // Method 2  -> Using Min Heap
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S){
