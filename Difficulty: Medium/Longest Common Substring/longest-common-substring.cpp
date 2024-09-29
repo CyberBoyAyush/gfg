11e0eb7c86ff9beff92cef8b4fcd8f66b797e9d9
Ayush Sharma <aayush240204@gmail.com>
2024-09-29 19:42:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -10,30 +10,23 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int dp[n+1][m+1];
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
         
-        for(int i = 0; i<=n ; i++){
-            dp[i][0] = 0;
-        }
-        for(int j = 0; j<=m; j++){
-            dp[0][j] = 0;
-        }
+        int res = 0;
         
-        int ans = 0;
-        
-        for(int i = 1; i<=n ; i++){
+        for(int i = 1; i<=n; i++){
             for(int j = 1; j<=m; j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans = max(ans,dp[i][j]);
+                if(s[i-1] == t[j-1]){ // same
+                    dp[i][j] = 1 + dp[i-1][j-1]; // add 1 to prev same
+                    res = max(res, dp[i][j]);
                 }
-                else{
+                else{ // now same
                     dp[i][j] = 0;
                 }
             }
         }
         
-        return ans;
+        return res;
     }
 };
 
