6e30e65233c367c11019ccccf02b1e08c347843a
Ayush Sharma <aayush240204@gmail.com>
2024-10-10 02:22:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> longestIncreasingSubsequence(int n, vector<int>& nums) {
+        vector<int> dp(n,1);
+        vector<int> hash(n);
+        int maxi = 1;
+        int lastIdx = 0;
+
+        for(int i = 0; i<n ; i++){
+            hash[i] = i;
+            for(int prev = 0; prev<=i; prev++){
+                if(nums[i] > nums[prev] && 1 + dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+            if(dp[i] > maxi){
+                maxi = dp[i];
+                lastIdx = i;
+            }
+        }
+        vector<int> temp;
+        temp.push_back(nums[lastIdx]);
+        while(hash[lastIdx] != lastIdx){
+            lastIdx = hash[lastIdx];
+            temp.push_back(nums[lastIdx]);
+        }
+        reverse(temp.begin(),temp.end());
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vector<int> res = obj.longestIncreasingSubsequence(N, arr);
+        for (auto x : res)
+            cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
