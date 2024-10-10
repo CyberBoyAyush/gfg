a40d47b5ef6c3037384c7ae6259eafbd8dd847ea
Ayush Sharma <aayush240204@gmail.com>
2024-10-10 22:44:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -7,31 +7,42 @@ using namespace std;
 class Solution {
   public:
     vector<int> longestIncreasingSubsequence(int n, vector<int>& nums) {
-        vector<int> dp(n,1);
-        vector<int> hash(n);
-        int maxi = 1;
-        int lastIdx = 0;
+        vector<int> dp(n, 1);    // dp[i] stores the length of LIS ending at index i
+        vector<int> hash(n);     // hash[i] stores the index of the previous element in LIS
+        
+        int maxLen = 1;          // Stores the length of the longest LIS found
+        int lastIdx = 0;         // Stores the last index of the LIS
 
-        for(int i = 0; i<n ; i++){
+        // Initialize the hash vector with each element pointing to itself
+        for(int i = 0; i < n; i++) {
             hash[i] = i;
-            for(int prev = 0; prev<=i; prev++){
-                if(nums[i] > nums[prev] && 1 + dp[prev] > dp[i]){
-                    dp[i] = 1 + dp[prev];
-                    hash[i] = prev;
+        }
+
+        // Calculate dp values and maintain the hash for LIS reconstruction
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    hash[i] = j;  // Mark j as the previous element of i in the LIS
                 }
             }
-            if(dp[i] > maxi){
-                maxi = dp[i];
+            // Update the maximum length and last index of the LIS
+            if(dp[i] > maxLen) {
+                maxLen = dp[i];
                 lastIdx = i;
             }
         }
+
+        // Reconstruct the LIS from the hash and lastIdx
         vector<int> temp;
-        temp.push_back(nums[lastIdx]);
-        while(hash[lastIdx] != lastIdx){
+        temp.push_back(nums[lastIdx]);  // Push the last element of the LIS
+        
+        while(hash[lastIdx] != lastIdx) {  // Trace back using the hash array
             lastIdx = hash[lastIdx];
-            temp.push_back(nums[lastIdx]);
+            temp.push_back(nums[lastIdx]);  // Push the corresponding element
         }
-        reverse(temp.begin(),temp.end());
+        
+        reverse(temp.begin(), temp.end());  // Reverse the sequence to get the correct order
         return temp;
     }
 };
