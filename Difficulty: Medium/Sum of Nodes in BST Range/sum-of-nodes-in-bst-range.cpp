fdf9b73dbc95d8f01cb194113a70456624499cea
Ayush Sharma <aayush240204@gmail.com>
2025-10-14 22:14:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Nodes in BST Range/sum-of-nodes-in-bst-range.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int nodeSum(Node* root, int l, int r) {
+        if (root == NULL) return 0;
+
+        // If current node is within the range, include its value
+        if (root->data >= l && root->data <= r) {
+            return root->data + nodeSum(root->left, l, r) + nodeSum(root->right, l, r);
+        }
+
+        // If node value is smaller than l, check only right subtree
+        else if (root->data < l) {
+            return nodeSum(root->right, l, r);
+        }
+
+        // If node value is greater than r, check only left subtree
+        else {
+            return nodeSum(root->left, l, r);
+        }
+    }
+};
